e1a5238f01086b390b7a310c227341e2345d62c1
sheimi <sheimi.zhang@gmail.com>
2014-01-21 10:05:33 -0800

    modify bench headfile

--- src/bench.h
+++ src/bench.h
@@ -4,23 +4,23 @@
 typedef unsigned long long int tick;
 
 #if defined(__i386__)
-static __inline__ tick rdtsc(void) {
+static inline tick rdtsc(void) {
   tick x;
-  __asm__ volatile (".byte 0x0f, 0x31" : "=A" (x));
+  asm volatile (".byte 0x0f, 0x31" : "=A" (x));
   return x;
 }
 #elif defined(__x86_64__)
-static __inline__ tick rdtsc(void) {
+static inline tick rdtsc(void) {
   unsigned hi, lo;
-  __asm__ __volatile__ ("rdtsc" : "=a"(lo), "=d"(hi));
+  asm volatile ("rdtsc" : "=a"(lo), "=d"(hi));
   return ((tick) lo) | (((tick) hi) << 32);
 }
 
 #elif defined(__powerpc__)
-static __inline__ tick rdtsc(void) {
+static inline tick rdtsc(void) {
   tick result=0;
   tick upper, lower,tmp;
-  __asm__ volatile(
+  asm volatile(
                 "0:                  \n"
                 "\tmftbu   %0           \n"
                 "\tmftb    %1           \n"
@@ -28,7 +28,7 @@ static __inline__ tick rdtsc(void) {
                 "\tcmpw    %2,%0        \n"
                 "\tbne     0b         \n"
                 : "=r"(upper),"=r"(lower),"=r"(tmp)
-                );
+              );
   result = upper;
   result = result << 32;
   result = result | lower;
@@ -36,7 +36,6 @@ static __inline__ tick rdtsc(void) {
   return (result);
 }
 #endif
-
 
 #endif
 
