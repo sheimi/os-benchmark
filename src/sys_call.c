22aa31ce4ae37ab29128d852df4685f0ff4812f1
zagfox <feng.zhu1991@gmail.com>
2014-01-21 19:37:49 -0800

    sys_call 3

--- src/sys_call.c
+++ src/sys_call.c
@@ -3,7 +3,7 @@
 #include "bench.h"
 
 #define LOOP_TIMES 10000
-int main() {
+void benchmark_sys_call() {
 	int i, pid, delta;
 	cpu_cycle before_call, after_call;
 	for (i = 0; i < LOOP_TIMES; i++) {
@@ -13,13 +13,11 @@ int main() {
 			getpid();
 			after_call = rdtsc();
 			delta = after_call - before_call;
-			printf("delta = %d\n", delta);
+			printf(stdout, "delta = %d\n", delta);
 			exit(0);
 		}
 		wait(NULL);
 	}
-
-
-	//printf("before_call = %lld\n", before_call);
-	return 0;
+	printf(stderr, "syscall overhead = \n");
 }
+
