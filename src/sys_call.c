32e6a0b00b61f49ae7a8bdebc429c2e3908b2bb2
zagfox <feng.zhu1991@gmail.com>
2014-01-26 22:07:09 -0800

    1-26

--- src/sys_call.c
+++ src/sys_call.c
@@ -7,10 +7,10 @@
 
 #define LOOP_TIMES 10000
 void benchmark_sys_call() {
-	int i, pid;
+	int i;
 	cpu_cycle before_call, after_call, delta;
 	for (i = 0; i < LOOP_TIMES; i++) {
-		/*pid = fork();
+		/*int pid = fork();
 		if (pid == 0) {*/
 			before_call = rdtsc();
 			syscall(SYS_getpid);
@@ -23,25 +23,4 @@ void benchmark_sys_call() {
 		wait(NULL);*/
 	}
 	fprintf(stderr, "syscall overhead complete, check output \n");
-  int i, pid, delta;
-  cpu_cycle before_call, after_call;
-  for (i = 0; i < LOOP_TIMES; i++) {
-    pid = fork();
-    if (pid < 0) {
-      fprintf(stderr, "fork error\n");
-      exit(0);
-    }
-    else if (pid == 0) {
-      before_call = rdtsc();
-      getpid();
-      after_call = rdtsc();
-      delta = after_call - before_call;
-      fprintf(stdout, "delta = %d\n", delta);
-      fflush(stdout);
-      exit(0);
-    }
-    wait(NULL);
-  }
-  fprintf(stderr, "syscall overhead complete, check output \n");
-}
-
+ } 
