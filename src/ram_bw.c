d193ef99c283d4d21ce110eaa9e494acf212354d
cse221 <cse221@cse221-ubuntu.(none)>
2014-02-16 12:34:40 -0800

    modify rambw.c

--- src/ram_bw.c
+++ src/ram_bw.c
@@ -2,12 +2,12 @@
 #include "bench.h"
 
 #define LOOP_TIMES 100
-#define DATA_SIZE 2
+#define DATA_SIZE 16
 #define STRIDE 1
 
 #define DO_SIXTEEN_TIMES(p) p;p;p;p;p;p;p;p;p;p;p;p;p;p;p;p;
 #define DO_READ(i) n=array[i]; i += STRIDE;
-#define DO_WRITE(i) array[i]=n; i += STRIDE;
+#define DO_WRITE(i) array[i]=n; n.data[0]++; i += STRIDE;
 
 typedef struct _dataType {
 	unsigned char data[DATA_SIZE];
@@ -15,9 +15,10 @@ typedef struct _dataType {
 
 void benchmark_ram_bw() {
 	cpu_cycle ts1, ts2;
-	dataType n, array[1024*1024];
+	dataType n, array[256];
 	int i, j;
-	for (j = 0; j < LOOP_TIMES; j++, i=0) {
+	printf("read begin");
+	for (j = 0,i=0; j < LOOP_TIMES; j++, i=0) {
 		ts1 = rdtsc();
 		DO_SIXTEEN_TIMES(DO_SIXTEEN_TIMES(DO_READ(i)));
 		ts2 = rdtsc();
