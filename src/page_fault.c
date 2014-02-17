8bf1dfd726fd83d4a9e780992bd30bc85ecc0b16
zagfox <feng.zhu1991@gmail.com>
2014-02-17 12:11:56 -0800

    modify page fault

--- src/page_fault.c
+++ src/page_fault.c
@@ -8,28 +8,37 @@
 #include <errno.h>
 #include "bench.h"
 
+#define FILL_TIMES 512
 #define LOOP_TIMES 100
+#define STEP 4096
 
 void dommap(int fd, int size) {
-	char c, *bufp;
-	int i, j;
-	cpu_cycle t1, t2; 
-	bufp = mmap(NULL, size, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0); 
-	if (bufp == (void *)-1)
+	char *bufp;
+	int i;
+	cpu_cycle t1, t2;
+	bufp = mmap(NULL, size, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
+	if (bufp == (void *)-1) {
 		fprintf(stderr, "mmap error: %s \n", strerror(errno));
+		exit(1);
+	}
+	for (i = 0; i < FILL_TIMES; i++) {
+		bufp[i*STEP] = 'a';
+	}
+	printf("fill memory complete\n");
+
 	// Start measuring
-	for (i = 0, j=0; i < LOOP_TIMES; i++,j+=4096) {
+	for (i = FILL_TIMES; i < FILL_TIMES+LOOP_TIMES; i++) {
 		t1 = rdtsc();
-		c = bufp[j];
+		bufp[i*STEP] = 'a';
 		t2 = rdtsc();
-		printf("%c, %lld\n", c, t2-t1);
-	}   
+		printf("page fault time %llu\n", t2-t1);
+	}
 	munmap(bufp, size);
 }
-void benchmark_page_fault() {
+int main() {
 	int fd, pagesize;
 	struct stat stat;
-	system("fallocate -l 1M file.txt");
+	//system("fallocate -l 1G file.txt");
 	if ((fd = open("file.txt", O_RDWR, 0)) < 0)
 		fprintf(stderr, "open file error\n");
 	fstat(fd, &stat);
@@ -39,6 +48,6 @@ void benchmark_page_fault() {
 
 	dommap(fd, stat.st_size);
 	close(fd);
-	system("rm file.txt");
+	//system("rm file.txt");
+	return 0;
 }
-
